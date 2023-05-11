4de00b5b2740ceffdd2252ce0c80e8d83620208f
Thiago Alves <thiagoluizmachadoalves@gmail.com>
2023-05-11 16:43:23 -0300

    Commit referente ao vídeo 09

---
+++ curso-angular/src/app/components/if-render/if-render.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-if-render',
+  templateUrl: './if-render.component.html',
+  styleUrls: ['./if-render.component.css']
+})
+export class IfRenderComponent implements OnInit {
+  canShow: boolean = false;
+  name: string = "Thiago";
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+    
+  }
+}
