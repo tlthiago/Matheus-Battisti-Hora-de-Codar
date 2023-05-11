2654f975aaef46512af34412e0c2535add5be9fa
Thiago Alves <thiagoluizmachadoalves@gmail.com>
2023-05-11 11:22:31 -0300

    Commit com atualização do código para aula

--- curso-angular/src/app/components/first-component/first-component.component.ts
+++ curso-angular/src/app/components/first-component/first-component.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-first-component',
   templateUrl: './first-component.component.html',
   styleUrls: ['./first-component.component.css']
 })
-export class FirstComponentComponent {
 
+export class FirstComponentComponent implements OnInit {
+  name: string = "Thiago";
+  age: number = 30;
+  job = "Programador";
+  hobbies = ["Correr", "Jogar", "Estudar"];
+  car = {
+    name: "Polo",
+    year: 2019
+  }
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+    
+  }
 }
