94a86ed1bb1e72895b61dea0809e1a661d9fced9
Thiago Alves <thiagoluizmachadoalves@gmail.com>
2023-05-12 15:02:42 -0300

    Commit da aula 17

--- curso-angular/src/app/app-routing.module.ts
+++ curso-angular/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FirstComponentComponent } from './components/first-component/first-component.component';
+import { ListRenderComponent } from './components/list-render/list-render.component';
+import { PipesComponent } from './components/pipes/pipes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: FirstComponentComponent},
+  {path: 'list', component: ListRenderComponent},
+  {path: 'pipes', component: PipesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
